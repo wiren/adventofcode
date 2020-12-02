f7aecebe8963721b6ab8e62652756d4c918ff4c1
tobwir <tobias@wiren.se>
2020-12-02 18:40:05 +0100

    Day 2

---
+++ 2020/day2.py
@@ -0,0 +1,26 @@
+#! /usr/bin/python3
+
+import funs
+
+def parse_line(l):
+    s = l.split(': ')
+    r = s[0].split(' ')
+    minmax = r[0].split('-')
+    return (int(minmax[0]), int(minmax[1]), r[1], s[1])
+
+def pwd_valid(min, max, chr, pwd):
+    cnt = pwd.count(chr)
+    return cnt >= min and cnt <= max
+
+def pwd_valid2(pos1, pos2, chr, pwd):
+    return (pwd[pos1 - 1] == chr) != (pwd[pos2 - 1] == chr)
+
+def count_valid(val_fun, rules):
+    return sum(map(lambda r: 1 if val_fun(*r) else 0, rules))
+
+# lines = funs.read_lines_from_file('testinput2')
+lines = funs.read_lines_from_file('input2')
+rules = list(map(lambda l: parse_line(l), lines))
+
+print(count_valid(pwd_valid, rules))
+print(count_valid(pwd_valid2, rules))
--- 2020/funs.py
+++ 2020/funs.py
@@ -3,3 +3,7 @@
 def read_nrs_from_file(f):
     file = open(f, 'r')
     return list(map(lambda x: int(x), file.readlines()))
+
+def read_lines_from_file(f):
+    file = open(f, 'r')
+    return map(lambda l: str.rstrip(l), file.readlines())
