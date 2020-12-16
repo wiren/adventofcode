10c15dff2baa18816979f820f950185266bee6ae
tobwir <tobias@wiren.se>
2020-12-16 22:31:34 +0100

    Some tidying. Trying to make the code a bit more compact...

--- 2020/day16.py
+++ 2020/day16.py
@@ -1,104 +1,55 @@
-from functools import *
-from itertools import *
-from computer import *
 import funs
-import copy
+from functools import reduce
+
+
+def valid(x):
+    return any([x for _min, _max in all_rules if _min <= x <= _max])
+
+
+def rule_match(i, min1, max1, min2, max2):
+    return all([min1 <= v <= max1 or min2 <= v <= max2 for v in [t[i] for t in valid_tkts]])
+
 
 # ls = funs.lines_from_file("16.intest")
 # ls = funs.lines_from_file("16.intest2")
 ls = funs.lines_from_file("16.in")
-# ls.append('')  # Add empty line if needed
 
-a1 = 0
-a2 = 0
-
-rules = {}
-ticket = []
-nearby = []
-
-section = 0
-
+rules, ticket, nearby, section = {}, [], [], 0
 for l in ls:
     if not l:
         section += 1
-
-    if l in ['your tickte:', 'nearby tickets:', '']:
         continue
-
+    if l in ['your ticket:', 'nearby tickets:']:
+        continue
     if section == 0:
         r = l.split(': ')
-        rules[r[0]] = r[1].split(' or ')
-
+        rules[r[0]] = [int(y) for sub in map(lambda x: x.split('-'), r[1].split(' or ')) for y in sub]
     if section == 1:
-        ticket = l.split(',')
-
+        ticket = [int(x) for x in l.split(',')]
     if section == 2:
-        nearby.append(l.split(','))
+        nearby.append([int(x) for x in l.split(',')])
 
-# print(rules)
-# print(ticket)
-# print(nearby)
+all_rules = list(reduce(lambda a, b: a + [b[:2], b[2:]], rules.values(), []))
+all_vals = [val for sublist in nearby for val in sublist]
 
-all_rules = []
-for key, val in rules.items():
-    all_rules.append(val[0].split('-'))
-    all_rules.append(val[1].split('-'))
-# print(all_rules)
+a1 = sum([x for x in all_vals if not valid(x)])
 
-all_vals = [int(val) for sublist in nearby for val in sublist]
-
-def valid(x):
-    for r in all_rules:
-        min, max = r
-        if int(min) <= x <= int(max):
-            return True
-    return False
-
-def invalid_tkt(tkt):
-    return not all([valid(int(x)) for x in tkt])
-
-a1 = sum(list(filter(lambda x: not valid(int(x)), all_vals)))
-
-valid_tkts = list(filter(lambda x: not invalid_tkt(x), nearby))
-
+# PART 2
+valid_tkts = [tkt for tkt in nearby if all([valid(t) for t in tkt])]
 fields = []
 for i in range(len(ticket)):
-    fields.append(set(rules.keys()))
-print(fields)
+    fields.append({key for key, val in rules.items() if rule_match(i, *val)})
 
-for t in valid_tkts:
-    for i in range(len(t)):
-        for key, val in rules.items():
-            # print(key, val)
-            min1, max1 = val[0].split('-')
-            min2, max2 = val[1].split('-')
-            # print(i, t, min1, max1, min2, max2)
-            if not (int(min1) <= int(t[i]) <= int(max1) or int(min2) <= int(t[i]) <= int(max2)):
-                # print(fields[i], key, fields[i] - {key})
-                fields[i] = fields[i] - {key}
+while True:
+    ones = {next(iter(f)) for f in fields if len(f) == 1}
+    rem = [f for f in enumerate(fields) if len(f[1]) > 1]
+    if len(rem) == 0:
+        break
+    for i, r in rem:
+        fields[i] = r - ones
 
-def done(fs):
-    return all([len(f) == 1 for f in fs])
-
-print(fields)
-
-while not done(fields):
-    ones = set()
-    for f in fields:
-        if len(f) == 1:
-            ones.add(list(f)[0])
-    print(ones)
-    for i in range(len(fields)):
-        if len(fields[i]) > 1:
-            fields[i] = fields[i] - ones
-    print(fields)
-
-print(fields)
-
-a2 = 1
-for i in range(len(ticket)):
-    if 'departure' in list(fields[i])[0]:
-        a2 *= int(ticket[i])
+deps = filter(lambda x: 'departure' in x[1], map(lambda f: (f[0], list(f[1])[0]), enumerate(fields)))
+a2 = reduce(lambda a, b: a*b, map(lambda v: int(ticket[v[0]]), deps))
 
 print('\nRes 1:', a1)
 print('Res 2:', a2)
